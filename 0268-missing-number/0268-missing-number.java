6e17305c9b151694d9d351eb87e9df519b7a3efc
Kalisvar <147804678+Kalisvar@users.noreply.github.com>
2024-05-17 16:06:40 +0530

    Update 0268-missing-number.java

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,19 +1,30 @@
 class Solution {
     public int missingNumber(int[] nums) {
-     int[] hash = new int[nums.length+1];
-     for(int i = 0;i<nums.length;i++)
-     {
-        hash[nums[i]] ++;
-     }
+    //  int[] hash = new int[nums.length+1];
+    //  for(int i = 0;i<nums.length;i++)
+    //  {
+    //     hash[nums[i]] ++;
+    //  }
 
-     for(int i = 0;i<hash.length;i++)
-     {
-        if(hash[i]==0)
-        {
-            return i;
-        }
-     }
-    return -1;
+    //  for(int i = 0;i<hash.length;i++)
+    //  {
+    //     if(hash[i]==0)
+    //     {
+    //         return i;
+    //     }
+    //  }
+    // return -1;
+
+    int  xor1 = 0;
+    int  xor2 = 0;
+    for(int i =0;i<nums.length;i++)
+    {
+        xor2 = xor2 ^ nums[i];
+        xor1 = xor1^i;
+    }
+    xor1 = xor1 ^ nums.length;
+    return xor1^xor2;
+
 
     }
 }
