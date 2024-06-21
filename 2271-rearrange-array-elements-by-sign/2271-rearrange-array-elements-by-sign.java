5e2bb83d9f1005819148fe086d3d16fd8592bdd7
Kalisvar <147804678+Kalisvar@users.noreply.github.com>
2024-06-21 09:35:09 +0530

    Update 2271-rearrange-array-elements-by-sign.java

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-       int[] result = new int[nums.length];
+    int[] result = new int[nums.length];
        int pos = 0;
        int neg = 1;
        for(int i = 0; i<nums.length;i++)
@@ -15,11 +15,7 @@ class Solution {
             result[neg] = nums[i];
             neg += 2;
         }
-
-
-       }
-
-       
-        return result;
+    }
+       return result;
 }
 }
