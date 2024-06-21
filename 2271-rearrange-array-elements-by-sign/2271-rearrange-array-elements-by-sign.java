b95c6d0a5de00d860060632362e609218117ac26
Kalisvar <147804678+Kalisvar@users.noreply.github.com>
2024-06-21 09:20:57 +0530

    Time: 3 ms (100%), Space: 76.6 MB (75.35%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,40 +1,25 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int[] a = new int[nums.length/2];
-        int[] b = new int[nums.length/2];
-        int p =0;
-        int n =0;
-        for(int i = 0; i < nums.length;i++)
+       int[] result = new int[nums.length];
+       int pos = 0;
+       int neg = 1;
+       for(int i = 0; i<nums.length;i++)
+       {
+        if(nums[i] > 0 )
         {
-            
-            if(nums[i] > 0 &&  p < a.length)
-            {
-                a[p] = nums[i];
-                p +=1;
-            }
-            if (nums[i] < 0 && n < b.length)
-            {
-                b[n] = nums[i];
-                n+=1;
+            result[pos] = nums[i];
+            pos +=2 ;
+        }
+        else
+        {
+            result[neg] = nums[i];
+            neg += 2;
+        }
 
-            }
-        }
-        p =0;
-        n =0;
-        for(int i = 0;i<nums.length;i++)
-        {
-            if ( i % 2 != 0 && n < a.length)
-            {
-                nums[i] = b[n];
-                n+=1;
-            }
-           else  if( i % 2 == 0 &&  p < a.length )
-            {
-                nums[i] = a[p];
-                p +=1;
-            }
+
+       }
+
        
-        }
-        return nums;
-   }
+        return result;
+}
 }
