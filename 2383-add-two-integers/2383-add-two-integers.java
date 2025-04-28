88f7fe3fb4477773b31f6cd41ff40290923ad688
Kalisvar <147804678+Kalisvar@users.noreply.github.com>
2025-04-28 22:34:23 +0530

    Time: 0 ms (100%), Space: 41.2 MB (7.4%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.java
+++ 2383-add-two-integers/2383-add-two-integers.java
@@ -1,5 +1,18 @@
 class Solution {
     public int sum(int num1, int num2) {
-        return num1+num2;
+        // return num1+num2;
+        // by bit wise operation
+        // while(num2!=0){
+        //     int carry = num1 & num2;
+        //     num1 = num1 ^ num2;
+        //     num2 = carry << 1;
+        // }
+        // return num1;
+
+        // BY recusrive way
+        if(num2 == 0){
+            return num1;
+        }
+        return sum(num1 ^ num2,(num1 & num2) << 1);
     }
 }
