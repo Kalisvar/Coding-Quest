b00a1d779a6a2c2dbbac5a378f02de3008c84683
Kalisvar <147804678+Kalisvar@users.noreply.github.com>
2025-05-13 23:17:57 +0530

    Time: 0 ms (100%), Space: 40.6 MB (79.64%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,10 +1,16 @@
 class Solution {
     public int trailingZeroes(int n) {
-        int ans = 0;
-        int denom = 5;
-        while(denom <= n){
-            ans = ans + n / denom;
-            denom = denom * 5;
+        // int ans = 0;
+        // int denom = 5;
+        // while(denom <= n){
+        //     ans = ans + n / denom;
+        //     denom = denom * 5;
+        // }
+        // return ans;
+        int ans =0;
+        while(n!=0){
+            n = n/5;
+            ans = ans + n;
         }
         return ans;
     }
