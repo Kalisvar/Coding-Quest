2066dc6a4dddc3a486eb163373c016ceaa900ed5
Kalisvar <147804678+Kalisvar@users.noreply.github.com>
2026-02-24 23:38:29 +0530

    Time: 0 ms (100%), Space: 43.1 MB (53.57%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,17 +1,22 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        int startAscii = 97;
-        int[] alphabetsoccur = new int[26];
+        // int startAscii = 97;
+        // int[] alphabetsoccur = new int[26];
         if(sentence.length() <=25){
             return false;
         }
-        for(int i=0;i<sentence.length();i++){
-            int ascii = sentence.charAt(i);
-            alphabetsoccur[ascii-startAscii] = 1; 
-        }
+        // for(int i=0;i<sentence.length();i++){
+        //     int ascii = sentence.charAt(i);
+        //     alphabetsoccur[ascii-startAscii] = 1; 
+        // }
 
-        for(int i=0;i<alphabetsoccur.length;i++){
-            if(alphabetsoccur[i] == 0){
+        // for(int i=0;i<alphabetsoccur.length;i++){
+        //     if(alphabetsoccur[i] == 0){
+        //         return false;
+        //     }
+        // }
+        for(char i ='a';i<='z';i++){
+            if(sentence.indexOf(i) < 0){
                 return false;
             }
         }
