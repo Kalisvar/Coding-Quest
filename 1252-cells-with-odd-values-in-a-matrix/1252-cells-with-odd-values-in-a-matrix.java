e8e9b927db522fa89660c09a4595a53e3cb47ce0
Kalisvar <147804678+Kalisvar@users.noreply.github.com>
2026-02-25 23:33:41 +0530

    Time: 0 ms (100%), Space: 43.3 MB (79.8%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -1,40 +1,58 @@
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
-        int[][] result = new int[m][n];
-        int count =0;
-        for(int i=0;i<indices.length;i++){
-            for(int j = 0;j<indices[0].length;j++){
-                if(j==0){
-                    func(0,indices[i][j],result);
-                }
-                else{
-                    func(1,indices[i][j],result);
-                }
-            }
+        // int[][] result = new int[m][n];
+        // int count =0;
+        // for(int i=0;i<indices.length;i++){
+        //     for(int j = 0;j<indices[0].length;j++){
+        //         if(j==0){
+        //             func(0,indices[i][j],result);
+        //         }
+        //         else{
+        //             func(1,indices[i][j],result);
+        //         }
+        //     }
         
             
-        }
-        for(int i=0;i<result.length;i++){
-            for(int j =0;j<result[0].length;j++){
+        // }
+        // for(int i=0;i<result.length;i++){
+        //     for(int j =0;j<result[0].length;j++){
                
-                if(result[i][j] % 2 !=0){
-                    count ++;
-                }
-            } 
+        //         if(result[i][j] % 2 !=0){
+        //             count ++;
+        //         }
+        //     } 
+        // }
+        int oddRows=0 ;
+        int oddCol =0;
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+        for(int[] a:indices){
+            rows[a[0]]++;
+            cols[a[1]]++;
         }
-        return count;
-    }
-    public static void func(int type,int val,int[][] arr){
-        if(type == 0){
-            for(int i = 0;i<arr[0].length;i++){
-                arr[val][i] ++;
+        for(int i =0;i<m;i++){
+            if(rows[i]%2 ==1){
+                oddRows++;
             }
         }
-        else{
-            for(int i=0;i<arr.length;i++){
-                arr[i][val]++;
+        for(int i =0;i<n;i++){
+            if(cols[i]%2==1){
+                oddCol++;
             }
         }
+        return oddRows * (n-oddCol) + (m-oddRows)*oddCol;
     }
+    // public static void func(int type,int val,int[][] arr){
+    //     if(type == 0){
+    //         for(int i = 0;i<arr[0].length;i++){
+    //             arr[val][i] ++;
+    //         }
+    //     }
+    //     else{
+    //         for(int i=0;i<arr.length;i++){
+    //             arr[i][val]++;
+    //         }
+    //     }
+    // }
 
 }
